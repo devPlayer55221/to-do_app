cc38286a653d82e9154e792feffa715322b3ae0b
devPlayer55221 <mukundkedia3@gmail.com>
2020-10-10 13:46:39 +0530

    todo app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tp7!f1wx1am=)n&0(s%1=$l%mjt8v+fo+&bg*1k2)!h7%029ko'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
